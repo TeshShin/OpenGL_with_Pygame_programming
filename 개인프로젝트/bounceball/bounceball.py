7cc6ffe2fb5a87e6104c886cb7a9ac01c5ade6b9
TeshShin <shintom1222@gmail.com>
2023-11-01 05:12:58 +0900

    종료문 수정

--- 개인프로젝트/bounceball/bounceball.py
+++ 개인프로젝트/bounceball/bounceball.py
@@ -460,7 +460,6 @@ while True:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q: # 끄기
                         pygame.quit()
