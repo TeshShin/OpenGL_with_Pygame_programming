288f3b462af0d7007ab54bf745bac6f045fbb725
TeshShin <shintom1222@gmail.com>
2023-11-27 03:18:08 +0900

    SAT로 충돌 판정 적용 성공

--- 개인프로젝트/MakeSATproject/main.py
+++ 개인프로젝트/MakeSATproject/main.py
@@ -1,53 +1,103 @@
 import pygame
-from pygame.locals import *
-from pygame import Vector2
+import sys
+import math
 
-HUGE = 100000
-
-class polygon():
-    def __init__(self,points):
-        self.points = points
-        self.rotation_angle = 0
-        self.x = 0
-        self.y = 0
-        self.vx = 0
-        self.vy = 0
-        self.angular_velocity = 0
-        
-def SATtest(axis : Vector2, pointset : Vector2, minAlong : float, maxAlong : float):
-    minAlong = HUGE, maxAlong = -HUGE
-    for i in range(0, len(pointset)):
-        # 이 축에 따른 최소/최대를 얻기 위해 내적을 함.
-        dotval = pointset[i].dot(axis)
-        if(dotval < minAlong): minAlong = dotval
-        if(dotval > maxAlong): maxAlong = dotval
-    return axis, pointset, minAlong, maxAlong
-
-# 두 도형은 CONVEX HULLS 이어야함.
-def intersects(shape1 : polygon, shape2 : polygon):
-    for i in range(0, len(shape1.normals)):
-        shape1.normals[i], shape1.points, shape1Min, shape1Max = SATtest(shape1.normals[i], shape1.points, shape1Min, shape1Max)
-        shape1.normals[i], shape2.points, shape2Min, shape2Max = SATtest(shape1.normals[i], shape2.points, shape2Min, shape2Max)
-        if (not overlaps(shape1Min, shape1Max, shape2Min, shape2Max)):
-            return 0 # 교차점 없음
-        # 그렇지 않으면 다음 테스트로 넘어감
-    for i in range(0, len(shape2.normals)):
-        shape2.normals[i], shape1.points, shape1Min, shape1Max = SATtest(shape2.normals[i], shape1.points, shape1Min, shape1Max)
-        shape2.normals[i], shape2.points, shape2Min, shape2Max = SATtest(shape2.normals[i], shape2.points, shape2Min, shape2Max)
-        if (not overlaps(shape1Min, shape1Max, shape2Min, shape2Max)):
-            return 0 # 교차점 없음
-        # 모든 축에 대해서 오버랩이 발생했다면, 두 물체는 충돌한 것이다.
-    return 1
-        
-def overlaps(min1 : float, max1 : float, min2 : float, max2 : float):
-    return isBetweenOrdered(min2,min1,max1) or isBetweenOrdered(min1, min2, max2)
-
-def isBetweenOrdered(val : float, lowerBound : float, upperBound : float):
-    return lowerBound <= val and val <= upperBound
-
+# Pygame 초기화
 pygame.init()
 
-screen = pygame.display.set_mode((500,500))
+# 화면 크기 및 색상 설정
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("SAT Collision Detection")
+# 시간 설정
+clock = pygame.time.Clock()
+FPS = 60 # 시간 당 프레임
+# 색상 정의
+white = (255, 255, 255)
+red = (255, 0, 0)
+
+# 다각형 정점 좌표
+polygon1 = [[100, 100], [200, 100], [150, 200]]
+polygon2 = [[300, 150], [400, 150], [350, 250]]
+
+def project_polygon(axis, polygon):
+    """다각형을 축에 사영한 결과를 반환합니다."""
+    min_proj = float('inf')
+    max_proj = float('-inf')
+
+    for point in polygon:
+        dot_product = axis[0] * point[0] + axis[1] * point[1]
+        min_proj = min(min_proj, dot_product)
+        max_proj = max(max_proj, dot_product)
+
+    return min_proj, max_proj
+
+def get_axes(polygon):
+    """다각형의 변에 대한 축을 반환합니다."""
+    axes = []
+    for i in range(len(polygon)):
+        point1 = polygon[i]
+        point2 = polygon[(i + 1) % len(polygon)]
+        edge = [point1[0] - point2[0], point1[1] - point2[1]]
+        normal = [edge[1], -edge[0]]
+        length = math.sqrt(normal[0]**2 + normal[1]**2)
+        axis = [normal[0] / length, normal[1] / length]
+        axes.append(axis)
+
+    return axes
+
+def check_collision(polygon1, polygon2):
+    """두 다각형이 충돌하는지 여부를 확인합니다."""
+    axes = get_axes(polygon1) + get_axes(polygon2)
+
+    for axis in axes:
+        min_proj1, max_proj1 = project_polygon(axis, polygon1)
+        min_proj2, max_proj2 = project_polygon(axis, polygon2)
+
+        if max_proj1 < min_proj2 or max_proj2 < min_proj1:
+            # 축 사이에 갭이 있으면 충돌하지 않음
+            return False
+
+    return True
+
+# 게임 루프
+while True:
+    clock.tick(FPS)  # 프레임 제한
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        for i in range(len(polygon1)):
+            polygon1[i][0] -= 1
+    if keys[pygame.K_RIGHT]:
+        for i in range(len(polygon1)):
+            polygon1[i][0] += 1
+    if keys[pygame.K_UP]:
+        for i in range(len(polygon1)):
+            polygon1[i][1] -= 1 
+    if keys[pygame.K_DOWN]:
+        for i in range(len(polygon1)):
+            polygon1[i][1] += 1 
+    # 충돌 감지
+    collision = check_collision(polygon1, polygon2)
+
+    # 화면 그리기
+    screen.fill(white)
+    pygame.draw.polygon(screen, red, polygon1)
+    pygame.draw.polygon(screen, red, polygon2)
+
+    # 충돌 시 색상 변경
+    if collision:
+        pygame.draw.polygon(screen, (0, 255, 0), polygon1)
+        pygame.draw.polygon(screen, (0, 255, 0), polygon2)
+
+    pygame.display.flip()
+
+# 코드 실행
+pygame.quit()
 
 # TO-DO : 폴리곤 두 개 만들고 하나에 대해서 키보드의 입력에 따라 이동함.
 # 그리고 두 물체가 충돌했을때 충돌했다고 알람이 뜨도록 함.
